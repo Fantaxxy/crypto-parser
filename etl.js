7e53e5634601d665f432daf2a800c57ff646ec3d
Fantaxxy <byrdmanfun@gmail.com>
2022-02-02 19:24:18 -0500

    added etl

---
+++ etl.js
@@ -0,0 +1,10 @@
+const {load} = require('csv-load-sync');
+const bitcoin = load('coin_Bitcoin.csv');
+const dogecoin = load('coin_Dogecoin.csv');
+const ethereum = load('coin_Ethereum.csv');
+
+console.log(bitcoin[0])
+console.log(dogecoin[0])
+console.log(ethereum[0])
+
+// uses a different lobby to turn the csv into an array of objects
